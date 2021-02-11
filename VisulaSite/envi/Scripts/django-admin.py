bc1d412f2ec7167c2d17b0826ebe008b82ef6ef0
ab-80 <63623441+ab-80@users.noreply.github.com>
2021-02-10 21:34:47 -0600

    Re-initialized source code.  Changed include fixing text size and color.

---
+++ VisulaSite/envi/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\andrewbergerson\source\repos\ProductionVisula\VisulaSite\VisulaSite\envi\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
